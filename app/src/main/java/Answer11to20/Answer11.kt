10c6ebb31736375ac2b9a3d0b9acae3c11402872
Karox1234 <zzzcomet@gmail.com>
2023-12-11 09:53:36 +0900

    12/11

---
+++ app/src/main/java/Answer11to20/Answer11.kt
@@ -0,0 +1,17 @@
+package Answer11to20
+
+class Solution {
+    fun solution(num: Int): String {
+        var answer = ""
+        if(num % 2 == 0){
+            answer = "Even"
+        }
+        else{
+            answer = "Odd"
+        }
+        return answer
+    }
+}
+
+
+https://school.programmers.co.kr/learn/courses/30/lessons/12937
---
+++ app/src/main/java/com/example/codekata/Answer10.kt
@@ -0,0 +1,14 @@
+package com.example.codekata
+
+class Solution {
+    fun solution(numbers: IntArray): Double {
+
+        return numbers.average()
+    }
+}
+
+
+
+
+
+https://school.programmers.co.kr/learn/courses/30/lessons/120817
